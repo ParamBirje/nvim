82c8355799e35acfdda3e96df7ad574b4017d730
Param Birje <parambirje@gmail.com>
2024-07-24 17:48:11 +0530

    Disabled lazy.nvim's change notification

--- lua/birje/lazy.lua
+++ lua/birje/lazy.lua
@@ -1,4 +1,6 @@
+
 -- bootstrapping lazy.nvim package manager
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -12,4 +14,11 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("birje.plugins")
+require("lazy").setup("birje.plugins", {
+
+  -- disables the 'change detected, press enter to reload' dialog
+  change_notification = {
+    notify = false,
+  },
+
+})
